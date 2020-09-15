e7ce84e5c90a68f0e3019b6978217bc26029b14d
Alissa Champigny <achampigny4@gmail.com>
2020-09-15 10:28:28 -0400

    mysql connection created

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//Dependency
+const mysql = require('mysql');
+
+//Set up for mysql connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM to use.
+module.exports = connection;
