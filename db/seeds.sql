ba64941e2006d08641f234c3984b26a03680d66c
Alissa Champigny <achampigny4@gmail.com>
2020-09-15 10:18:11 -0400

    3 burgers added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Caramelized Onion BBQ Bacon Burger with Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
