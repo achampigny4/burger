35416e83873c521afdf5ba3542fe391a1dfdbcbb
Alissa Champigny <achampigny4@gmail.com>
2020-09-16 14:01:22 -0400

    controller.js created, required express, burger.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const burger = require('../models/burger');
+
+// 3. Inside the `burgers_controller.js` file, import the following:
+// ​
+//    * Express
+//    * `burger.js`
+// ​
+// 4. Create the `router` for the app, and export the `router` at the end of your file.
