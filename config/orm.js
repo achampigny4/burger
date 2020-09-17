1691eec7cc173399b822becaff3e7d63dbea0474
Alissa Champigny <achampigny4@gmail.com>
2020-09-17 10:57:52 -0400

    selectAll

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 //Connection Dependency
-const connection = require('./connection');
- ​
+let connection = require('./connection');
+
 // * In the `orm.js` file, create the methods
 //that will execute the necessary MySQL commands
 //in the controllers. These are the methods you
@@ -12,6 +12,35 @@ const connection = require('./connection');
 //      * `updateOne()`
 //      * `deleteOne()`
 
+//object of mySQL functions
+orm = {
+    //Read All Burgers
+    //these will be called in burger.js
+    selectAll: function (table, col, valOfCol) {
+        return new Promise((resolve, reject) => {
+            let queryString = "SELECT * FROM ?? ;";
+            connection.query(queryString, [table, col, valOfCol], function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+                //////////////////
+                console.log(result);
+                /////////////////
+            });
+        });
+    },
+    //Create Burger
+//     insertOne: function () {
+//         let queryString = "";
+//     },
+//     //Update Burger
+//     updateOne: function () {
+//         let queryString = "";
+//     },
+//     //Delete Burger
+//     deleteOne: function () {
+//         let queryString = "";
+//     }
+};
 
 // Export the orm object for the model
 module.exports = orm;
