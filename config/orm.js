a0ce04910574c8823eaa2df2bd697bf615eade3e
Alissa Champigny <achampigny4@gmail.com>
2020-09-17 13:46:17 -0400

    deleteOne() , var to let

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,7 @@ let connection = require('./connection');
 
 // create an array of question marks and turns it into a string.
 function printQuestionMarks(num) {
-    var arr = [];
+    let arr = [];
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
@@ -16,10 +16,10 @@ function printQuestionMarks(num) {
 
 // convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-    var arr = [];
+    let arr = [];
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-        var value = ob[key];
+        let value = ob[key];
         // check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
             // if string with spaces, add quotations
@@ -51,7 +51,7 @@ orm = {
     },
     //Create Burger
     insertOne: function (table, cols, valOfCol, cb) {
-        var queryString = "INSERT INTO " + table;
+        let queryString = "INSERT INTO " + table;
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
@@ -71,7 +71,7 @@ orm = {
     },
     //Update Burger
     updateOne: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+        let queryString = "UPDATE " + table;
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
@@ -87,10 +87,21 @@ orm = {
             cb(result);
         });
     },
-    //Delete Burger
-    //     deleteOne: function () {
-    //         let queryString = "";
-    //     }
+    // Delete Burger
+    deleteOne: function (table, condition, cb) {
+        let queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+        ////////////////////////
+        console.log("deleteOne queryString here:" + queryString);
+        ////////////////////////
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 };
 
 // Export the orm object for the model
