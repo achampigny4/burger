c97cabf8768a7725180412bd33a1921b16a7266b
Alissa Champigny <achampigny4@gmail.com>
2020-09-18 15:57:34 -0400

    revert back to drop,create, use

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
 USE burgers;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(75) NOT NULL,
     createdAt TIMESTAMP NOT NULL,
-	 devoured BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
 
