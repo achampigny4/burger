cd23f5f34bd81d30a0465d48409fd2555a7578c0
Alissa Champigny <achampigny4@gmail.com>
2020-09-18 11:36:20 -0400

    previous code and notes revomed

--- models/burgers.js
+++ models/burgers.js
@@ -1,27 +1,3 @@
-// Dependency require orm functions
-// const burgers = require('../config/orm');
-
- //Promise stream of data
-//  burgers.selectAll("burgers", "burger_name", "devoured").then(function (result) {
-//      let data = result;
-//      console.log(data);
-//  });
-
-//  burgers.insertOne(cols, valOfCol).then(function (result) {
-//     let data = result;
-//     console.log(data);
-// });
-
-//  updateOne('', '').then(function (result) {
-//     let data = result;
-//     console.log(data);
-// });
-
-//  deleteOne('', '').then(function (result) {
-//     let data = result;
-//     console.log(data);
-// });
-
 //Dependency connection and ORM
 const dbManager = require("../config/dbManager");
 
