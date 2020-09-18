62beb016ad70aee1dae3e880ae722177abe994fd
Alissa Champigny <achampigny4@gmail.com>
2020-09-18 14:19:42 -0400

    jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,10 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
+USE y3a9r3nap7da1f9o;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(75) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+	 devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
 
