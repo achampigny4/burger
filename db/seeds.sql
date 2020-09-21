f4cd0f2c36aed2c88e0a6dd6505ec465322fbbd0
Alissa Champigny <achampigny4@gmail.com>
2020-09-20 21:36:33 -0400

    spooky Bob's Burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Caramelized Onion BBQ Bacon Burger with Avocado', 1);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Swiss Burger', 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried-egg the 13th Burger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Creature from the Black Legume Burger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spider Sliders', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rise of the Munster Burger', 0);
